75e96df3435aa2e321e16831722caa6eb8876f4a
tommaster-pixel <tomjerry360degree@gmail.com>
2021-04-16 13:36:12 +0530

    Add All files

---
+++ data.js
@@ -0,0 +1,292 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living-room",
+      "name": "living room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": 0.026133755865906494,
+        "pitch": -0.09056030524380354,
+        "fov": 1.265676047841681
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2661759728344109,
+          "pitch": 0.012268125337570268,
+          "rotation": 0,
+          "target": "3-pooja-room"
+        },
+        {
+          "yaw": 2.1139518990092103,
+          "pitch": -0.04108884710040961,
+          "rotation": 0,
+          "target": "1-dining"
+        },
+        {
+          "yaw": 0.8917014492062147,
+          "pitch": -0.050413988050138414,
+          "rotation": 0,
+          "target": "4-bed-room"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7668256988240145,
+          "pitch": 0.01778393392438815,
+          "title": "staircase",
+          "text": "this is renovation project staircase is left as it is as per clients wish&nbsp;"
+        },
+        {
+          "yaw": 2.86656053066471,
+          "pitch": -0.07646878394994872,
+          "title": "bed room",
+          "text": "similar"
+        },
+        {
+          "yaw": 1.8366912637340889,
+          "pitch": -0.6587180481232195,
+          "title": "false ceiling",
+          "text": "this ceiling is full of maths"
+        },
+        {
+          "yaw": 0.3817430904983148,
+          "pitch": -1.362224311823045,
+          "title": "formula",
+          "text": "this is a formula to find area between two curve ."
+        },
+        {
+          "yaw": -0.25754766836349674,
+          "pitch": -0.11735465277928192,
+          "title": "Tv Unit",
+          "text": "wall mounted wooden ply with floating table .ply is mica and veneer finish with glossy resin coating. Since there is a window client need to close window as per Vaastu so we provided such a elegant simple design&nbsp;&nbsp;"
+        },
+        {
+          "yaw": -2.868379790929218,
+          "pitch": 1.0148553044076074,
+          "title": "sofa",
+          "text": "l shape sofa(LHS)"
+        },
+        {
+          "yaw": -1.5632925768173944,
+          "pitch": -0.2827870095215257,
+          "title": "CURTAIN",
+          "text": "Though the window is small client prefers to be rich look&nbsp; ."
+        },
+        {
+          "yaw": 0.9281125311146177,
+          "pitch": 0.38591986319717364,
+          "title": "TILES",
+          "text": "this is already provided tiles"
+        },
+        {
+          "yaw": 1.357413039216583,
+          "pitch": -0.3226730537973381,
+          "title": "wall paint",
+          "text": "Asian paint&nbsp; We will provide code to client at the time of project submission"
+        },
+        {
+          "yaw": -2.905431843867749,
+          "pitch": -0.1559902128241184,
+          "title": "mirror",
+          "text": "2 feet circular mirror with backside&nbsp; hidden light mounted on bamboo painted&nbsp; with veneer texture glossy finish"
+        }
+      ]
+    },
+    {
+      "id": "1-dining",
+      "name": "dining",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7664491512224192,
+          "pitch": 0.20663678447139233,
+          "rotation": 0,
+          "target": "2-kitchen"
+        },
+        {
+          "yaw": 1.8874098117181815,
+          "pitch": 0.6918080292893283,
+          "rotation": 0,
+          "target": "0-living-room"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.28218468744233505,
+          "pitch": 0.6851568476547012,
+          "title": "dining table",
+          "text": "modified already avilable dining table"
+        }
+      ]
+    },
+    {
+      "id": "2-kitchen",
+      "name": "kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.354464394691192,
+          "pitch": 0.031604295727706955,
+          "rotation": 0,
+          "target": "1-dining"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-pooja-room",
+      "name": "pooja room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.92074488511226,
+          "pitch": 0.04562909213026778,
+          "rotation": 0,
+          "target": "0-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bed-room",
+      "name": "bed room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6808432065771193,
+          "pitch": 0.16201953517397527,
+          "rotation": 0,
+          "target": "0-living-room"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.961914017245327,
+          "pitch": -0.17549256586075934,
+          "title": "wadrobe",
+          "text": "this is technical glitch happen while rendering will update correct asap"
+        },
+        {
+          "yaw": -0.27403166636952747,
+          "pitch": -1.3147127692678318,
+          "title": "veneer",
+          "text": "same texture used in Tv unit"
+        },
+        {
+          "yaw": -0.14865371331783805,
+          "pitch": -0.2178961701183102,
+          "title": "3d&nbsp; pannel",
+          "text": "floral type"
+        },
+        {
+          "yaw": 0.599203371782302,
+          "pitch": 0.13860688036712787,
+          "title": "attached bathroom",
+          "text": "left as it is"
+        }
+      ]
+    }
+  ],
+  "name": "maths teacher",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
